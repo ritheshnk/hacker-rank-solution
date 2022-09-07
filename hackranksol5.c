106eaad03405179488bfc0888b8b770190a8d96c
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-09-07 17:17:46 +0530

    Add files via upload

---
+++ hackranksol5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main() 
+{
+    int a=0,b=0,n=0;
+    char* arr[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    for(n = a;n<=b;n++)  //starting from a till b
+    {
+        if(n>9)
+        {
+            if(n%2 ==0)
+               printf("even\n");
+            else {
+            printf("odd\n");
+            }   
+        }
+        else {
+          puts(arr[n]);
+        }
+    }
+    return 0;
+
+}
+
